b293bdaea89b2767cced606cb0a561e50d151705
clément chenebault <clem.chenebault@gmail.com>
2016-11-14 21:05:16 +0000

    dear

--- labs/clement/01/src/js/ViewLine.js
+++ labs/clement/01/src/js/ViewLine.js
@@ -5,6 +5,7 @@ import alfrid, { GL } from 'alfrid';
 
 
 import Line from './geom/Line'
+import Dear from './animals/Dear'
 import Spline from './libs/Spline';
 import vs from '../shaders/line.vert';
 import fs from '../shaders/line.frag';
@@ -23,65 +24,14 @@ class ViewLine extends alfrid.View {
     this.points = []
     this.spline = new Spline([]);
 
-    var pts = [
-			[813, 68, 0],
-			[788, 157, 0],
-			[725, 167, 0],
-			[667, 149, 0],
-			[670, 106, 0],
-			[709, 66, 0],
-			[745, 77, 0],
-			[762, 126, 0],
-			[743, 214, 0],
-			[697, 245, 0],
-			[649, 242, 0],
-			[607, 197, 0],
-			[588, 109, 0],
-			[578, 39, 0],
-			[572, 39, 0],
-			[519, 200, 0],
-			[534, 251, 0],
-			[764, 386, 0],
-			[810, 364, 0],
-			[813, 297, 0],
-			[752, 292, 0],
-			[715, 330, 0],
-			[707, 414, 0],
-			[709, 595, 0],
-			[709, 595, 0],
-			[660, 504, 0],
-			[575, 453, 0],
-			[493, 448, 0],
-			[433, 471, 0],
-			[394, 561, 0],
-			[372, 604, 0],
-			[372, 604, 0],
-			[347, 438, 0],
-			[289, 367, 0],
-			[303, 287, 0],
-			[350, 249, 0],
-			[435, 246, 0],
-			[495, 361, 0],
-			[473, 481, 0],
-			[448, 531, 0],
-			[446, 580, 0],
-			[456, 607, 0]
-		];
+
+		this.dear = new Dear();
 
 		this.tick = 0;
-		for (var i = 0; i < pts.length; i++) {
-
-			this.tick++;
-
-			pts[i][0] /= 300;
-			pts[i][1] /= 300;
-			pts[i][2] = Math.cos(this.tick);
-			pts[i][0] -= 2;
-			pts[i][1] -= 2;
-		}
 
 
-    this.finalP = this.getPoints(pts);
+
+    this.finalP = this.getPoints(this.dear.getPoints());
     this.line = new Line(this.finalP);
 
 

a011d522507c3c267137072fb896dccf52422d39
wen <bongiovi015@gmail.com>
2016-11-29 00:32:31 +0000

    copy projection matrix to vive cam

--- build/src/js/utils/GetReflectionMatrix.js
+++ build/src/js/utils/GetReflectionMatrix.js
@@ -38,6 +38,7 @@ const func1 = function(mCamera, mSeaLevel, mTarget) {
 	posTargetRefl[1] -= distToWater * 2.0;
 
 	mTarget.lookAt(posCamRefl, posTargetRefl);
+	mat4.copy(mTarget.projection, mCamera.projection);
 }
 
 
